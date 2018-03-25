2cfedb0bca73843e14a7ef562adcf3970cb84e04
gekiganga77@hotmail.com <gekiganga77@hotmail.com>
2018-03-25 19:32:54 +0900

    doc url fix

--- src/sendalert.cpp
+++ src/sendalert.cpp
@@ -57,7 +57,7 @@ void ThreadSendAlert(CConnman& connman)
     //  Higher numbers mean higher priority
     alert.nPriority     = 5000;
     alert.strComment    = "";
-    alert.strStatusBar  = "URGENT: Upgrade required: see https://www.wavi.org";
+    alert.strStatusBar  = "URGENT: Upgrade required: see Website";
 
     // Set specific client version/versions here. If setSubVer is empty, no filtering on subver is done:
     // alert.setSubVer.insert(std::string("/Wavi Core:0.12.0.58/"));
--- src/wavid.cpp
+++ src/wavid.cpp
@@ -27,7 +27,7 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Wavi (https://www.wavi.org/),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Wavi (https://wavidev-the-man.github.io/wavi/),
  * which enables instant payments to anyone, anywhere in the world. Wavi uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
